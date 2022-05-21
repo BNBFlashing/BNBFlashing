ebf24b676c4aee16696d2c6b44b70e0ab56481aa
BNBFlashing <lokea695@gmail.com>
2022-05-21 19:49:28 -0400

    BNBFlashing Commit

---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  chainId: 97,
+  contractAddress: "0x189808A11dba4045833A6eFe599beEE3556c7AA0",
+  scanLink:
+    "https://testnet.bscscan.com/address/0x189808a11dba4045833a6efe599beee3556c7aa0#code"
+};
